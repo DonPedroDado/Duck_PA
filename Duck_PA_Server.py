803490f6785beac327efa0d418a2c44e294e0832
DonPedroDado <136968346+DonPedroDado@users.noreply.github.com>
2025-02-01 13:44:44 +0100

    blabla

--- Duck_PA_Server.py
+++ Duck_PA_Server.py
@@ -2,36 +2,61 @@ from typing import Union
 from fastapi import FastAPI
 from ollama import chat
 from ollama import ChatResponse
-#import re
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+first_message={}
+
 def ask_AI(text: str):
     response: ChatResponse = chat(model='llama3.2:latest', messages=[
-       # {
-        #    "role": "system",
-       #     "content": "You are an assistant that helps students with different topics. You must explain the topics in an easy way.",
-        #},
         {
             "role": "user",
             "content": text,
         },
     ])
-    
-    #cleaned_content = re.sub(r"<think>.*?</think>\n?", "",
-    #response, flags=re.DOTALL)
 
-    #return cleaned_content
-    return response
+    return response["message"]["content"]
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Welcome": "to the AI"}
 
 
-@app.get("/first_prompt/{name}")
-def first_promt(name: str):
-    message=f"You are an AI assistant that now will be used to allow students to learn particular topics. The first thing we are going to do is to create a profile of the student. The name of the student is {name}. In the following you are going to ask question one by one and after each question I'm going to provide the answer. Once you are ready just reply to the last question with the string TYYUIOH."
+@app.get("/create_profile/{name}")
+def create_profile(name: str):
+    message=f"You are an AI assistant that now will be used to allow students to learn particular topics. You are talking to the student and he is called {name}. Our plan is the following, we will first ask questions to create his profile. You want to assess his degree level, age and other information that is pertinent to students. Once you are done creating the profile, you are going to reply with a full summary of the profile that can be used for next steps. Now ask questions one by one and after each question the student is going to profile the answer. Once you are ready, reply with the message <EOP> the summary of the profile <EOP>. You are going to ask at most 5 questions."
 
-    return {"name": ask_AI(message)}
+    first_message[name] = message
+
+    return {"text": ask_AI(message)}
+
+class ProfileContinuation(BaseModel):
+    name: str
+    messages: List[str]
+@app.post("/continue_profile_creation")
+
+
+def continue_profile_creation(data: ProfileContinuation):
+    if data.name not in first_message:
+        return {"error": "unknown user"}
+
+    messages=[{
+            "role": "user",
+            "content": first_message[data.name],
+        }]
+    
+
+    
+    for i in data.messages:
+        messages.append({
+            "role": "user",
+            "content": i,
+        })
+
+    print(messages)
+    response: ChatResponse = chat(model='llama3.2:latest', messages=messages)
+
+    return {"question": response["message"]["content"]}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Duck_TAI
-Here is all the code for the Duck TAI(Duck Test AI). It corrects tests(exams).
+Here is all the code for the Duck PA project that helps students understand new topics.
